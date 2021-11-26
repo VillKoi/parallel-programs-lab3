5d228f63fd819a24111c86951662676e15ac94b2
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-11-27 00:50:13 +0300

    Scripted auto-commit on change (2021-11-27 00:50:13) by gitwatch.sh

--- src/main/java/flights/FlightApp.java
+++ src/main/java/flights/FlightApp.java
@@ -1,16 +1,23 @@
 package flights;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class FlightApp {
     public static void main(String[] args) throws Exception {
         if (args.length != 3) {
-            System.err.println("Usage: AirportsApp <1: input path FlightMapper> <2: input path AirportMapper> <output path>");
+            System.err.println("Usage: FlightApp <1: input path FlightMapper> <2: input path AirportMapper> <output path>");
             System.exit(-1);
         }
 
         SparkConf conf = new SparkConf().setAppName("lab3");
-        JavaSparkContext sc = new JavaSparkContext(conf);
+        JavaSparkContext sctx = new JavaSparkContext(conf);
+
+        String flightMapperPath = args[0];
+        String airportMapperPath = args[1];
+        String outPath = args[2];
+
+        JavaRDD<String> flightRddRecords =  sctx.textFile(flightMapperPath)
     }
 }
