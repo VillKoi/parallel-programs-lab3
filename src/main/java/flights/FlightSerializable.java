9505c86a30fa8175d601184a9ad8a7072db53329
Вольнова Татьяна Михайловна <tmvolno1@mts.ru>
2021-11-27 02:57:20 +0300

    Scripted auto-commit on change (2021-11-27 02:57:20) by gitwatch.sh

--- src/main/java/flights/FlightSerializable.java
+++ src/main/java/flights/FlightSerializable.java
@@ -4,10 +4,20 @@ import java.io.Serializable;
 
 public class FlightSerializable implements Serializable {
     private float delayingTime;
+    private float maxDelayingTime;
     private int cancelled;
+    private int delaying;
 
     public FlightSerializable(float delayingTime, boolean isCancelled) {
-        this.delayingTime = delayingTime;
+        this.delayingTime += delayingTime;
+        if (delayingTime != 0) {
+            delaying++;
+        }
+
+        if (delayingTime > this.maxDelayingTime) {
+            this.maxDelayingTime = delayingTime;
+        }
+
         if (isCancelled) {
             this.cancelled++;
         }
